82e7ab1d47db56e40a6b79d0e0556fe9b9dcb560
denis <dovganichd@digicode.net>
2016-03-25 17:27:02 +0100

    Multiply page support

--- Assets/Game/Scripts/PageReplacer.cs
+++ Assets/Game/Scripts/PageReplacer.cs
@@ -19,8 +19,6 @@ public class PageReplacer : MonoBehaviour
 
 	public void ReplacePage(int pageNumber, int textNumber)
 	{
-//		book.onRebuild += OnRebuild;
-
 		TextPart textPart = textMaster.GetTextPart(textNumber);
 
 		//Delete old pages
@@ -38,37 +36,22 @@ public class PageReplacer : MonoBehaviour
 			pageBuilder.pages.Add(go);
 		}
 
-//		pageBuilder.pages = textPart.pages;
 
 		Debug.Log(pageNumber.ToString());
-		book.AttachDynamicObjectToPage(book.pages[pageNumber], 0);
-//		book.pages[0].obj = book.MakePageObject(book.pages[0], 0);
-//		book.BuildPageMeshes(); //It is working!
-//		book.pages[0].UpdateAttached(book.pageparams[0].objects[0], 0);
-//		book.MakePageObject
 
-//		book.rebuild = true;
+		for (int i = 0; i < pageBuilder.pages.Count; i++)
+		{
+			book.AttachDynamicObjectToPage(book.pages[pageNumber + i], i);
+		}
+
+	}
+
+//	void OnRebuild()
+//	{
+//		book.onRebuild -= OnRebuild;
+//
 //		book.rebuildmeshes = true;
-
-//		book.UpdateAttached();
-//		book.MakePageObject(book.pages[pageNumber], 0);
-//		book.UpdateBookMT();
-//		book.UpdateAttachObject(book.pages[pageNumber], book.);
-
-//		book.pages[pageNumber].UpdateAttached();
-
-//		Debug.Log(book.pages.Count.ToString());
-//		Debug.Log(book.pageparams.Count.ToString());
-
-	}
-
-	void OnRebuild()
-	{
-		book.onRebuild -= OnRebuild;
-
-//		book.rebuild = true;
-		book.rebuildmeshes = true;
-	}
+//	}
 
 	int GetPageNumber(int textNumber)
 	{
